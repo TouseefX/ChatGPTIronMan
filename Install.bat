8d8d56da210458334a73d26acf042ac93be257dc
TouseefX <131637263+TouseefX@users.noreply.github.com>
2024-01-06 18:56:43 +0400

    Needed

---
+++ Install.bat
@@ -0,0 +1,20 @@
+pip install platform
+pip install openai
+pip install speechrecognition
+pip install sr
+pip install pyttsx3
+pip install webbrowser
+pip install subprocess
+pip install pytube
+pip install requests
+pip install opencv-python
+pip install pyzbar
+pip install BeautifulSoup
+pip install bs4
+pip install google-cloud-storage
+pip install google-cloud
+pip install vision
+pip install google-cloud-vision
+echo Installed Moudles.
+echo Please Run Main.py In VS Code
+pause
---
+++ InstallForMacAndLinux.sh
@@ -0,0 +1,19 @@
+pip install platform
+pip install openai
+pip install speechrecognition
+pip install sr
+pip install pyttsx3
+pip install webbrowser
+pip install subprocess
+pip install pytube
+pip install requests
+pip install opencv-python
+pip install pyzbar
+pip install BeautifulSoup
+pip install bs4
+pip install google-cloud-storage
+pip install google-cloud
+pip install vision
+pip install google-cloud-vision
+echo Installed Please Run Main.py in VS Code
+pause
